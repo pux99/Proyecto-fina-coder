22d85682887467908f194ba4e7d71b4f661200b5
pux99 <ipuk@frba.frba.utn.edu.ar>
2022-05-24 23:26:03 -0300

    me voy a dormir

--- Assets/Script/GeneralScripts/UiManager.cs
+++ Assets/Script/GeneralScripts/UiManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UiManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Image healthBar,DashBar,manaBar;
+    public  TextMeshProUGUI quest;
     public float vidaActual,manaActual,DashCD,dashTimer;
     // Update is called once per frame
     void Update()
@@ -19,5 +21,7 @@ public class UiManager : MonoBehaviour
         healthBar.fillAmount = vidaActual / 100;
         manaActual = PlayerMovement.displayMana;
         manaBar.fillAmount = manaActual / 100;
+        //quest.text="Explora el bosque y encuentra la guarida de los goblinsXD";
+        quest.text = "Encuentra el amuleto robado";
     }
 }
