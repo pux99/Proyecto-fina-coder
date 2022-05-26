16968b95addcadd0180c84bf5fb36468f9344e57
pux99 <ipuk@frba.frba.utn.edu.ar>
2022-05-26 15:34:20 -0300

    unas cuabtas cosas

--- Assets/Script/ending.cs
+++ Assets/Script/ending.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ending : MonoBehaviour
 {
     public GameObject endscene;
-    public Transform camara;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/atackmenu/Scripts/AtaquesFisicos.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "nuevo Ataque Fisico", menuName = "AtaqueFisico")]
-public class AtaquesFisicos : TemplateAtaque
-{
-    public int fuerzaRequerida;
-}
